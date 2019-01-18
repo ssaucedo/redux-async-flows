3b6f6c5c5c87e9f21c14946a8470e6407f4a1871
Santiago Saucedo <santiago.g.saucedo@gmail.com>
2019-01-18 00:10:16 -0300

    fix(*): Fix tests, remove reducer

---
+++ src/index.ts
@@ -0,0 +1,41 @@
+interface Register {
+  [index:string]: Array<Function>
+}
+
+interface Action {
+  type: string
+  payload: object
+}
+
+interface Output{
+  take: Function,
+  asyncFlowsMiddleware: Function
+}
+
+function createAsyncFlowsMiddleware(): Output{
+  
+  const register: Register = {};
+
+  const take = (actionType: string) => {
+    return new Promise(resolve => {
+      register[actionType] = [...(register[actionType] || []), resolve];
+    });
+  };
+
+  return {
+    take,
+    asyncFlowsMiddleware: () => (next: Function) => (action: Action)=> {
+
+      if(register[action.type]) {
+        register[action.type].forEach(resolve => resolve(action));
+        register[action.type] = [];
+      }
+      return next(action);
+    }
+  };
+};
+
+export default createAsyncFlowsMiddleware;
+
+
+
