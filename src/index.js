bb73542ffea655982e9d1a5868206183625f50bc
Santiago Saucedo <santiago.g.saucedo@gmail.com>
2019-01-18 23:45:00 -0300

    feat(Middleware): Take returns rejected promise when actionType is not a string

--- src/index.js
+++ src/index.js
@@ -1,22 +1,27 @@
 function createAsyncFlowsMiddleware() {
-  
+
   const register = {};
 
-  const take = (action) => {
-    return new Promise(resolve => {
-      register[action] = [...(register[action] || []), resolve];
-    });
+  const take = (actionType) => {
+    if (typeof actionType === 'string') {
+      return new Promise(resolve => {
+        register[actionType] = [...(register[actionType] || []), resolve];
+      });
+    }
+    return Promise.reject(new Error(`Cannot take an ${typeof actionType}, string expected`))
   };
 
-  take.any = (...actions) => Promise.race(actions.map(take));
-  
-  take.all = (...actions) => Promise.all(actions.map(take))
+  take.any = (...actionTypes) => {
+    return Promise.race(actionTypes.map(take))
+  };
+
+  take.all = (...actionTypes) => Promise.all(actionTypes.map(take))
 
   return {
     take,
     asyncFlowsMiddleware: () => next => action => {
 
-      if(register[action.type]) {
+      if (register[action.type]) {
         register[action.type].forEach(resolve => resolve(action));
         register[action.type] = [];
       }
