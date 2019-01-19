eb4d62b294b809b813f7577afbce7777bfba4fba
Alan Campora <acampora@medallia.com>
2019-01-18 22:31:24 -0300

    style(redux-async-flows): adding typescript support

--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,35 @@
 interface Register {
-  [index:string]: Array<Function>
+  [index: string]: Array<Function>;
 }
 
 interface Action {
-  type: string
-  payload: object
+  type: string;
+  payload: object;
 }
 
-interface Output{
-  take: Function,
-  asyncFlowsMiddleware: Function
+interface Output {
+  take: Function;
+  asyncFlowsMiddleware: Function;
 }
 
-function createAsyncFlowsMiddleware(): Output{
-  
+function createAsyncFlowsMiddleware(): Output {
   const register: Register = {};
 
-  const take = (actionType: string) => {
-    return new Promise(resolve => {
+  const take = (actionType: string) =>
+    new Promise(resolve => {
       register[actionType] = [...(register[actionType] || []), resolve];
     });
-  };
 
   return {
     take,
-    asyncFlowsMiddleware: () => (next: Function) => (action: Action)=> {
-
-      if(register[action.type]) {
+    asyncFlowsMiddleware: () => (next: Function) => (action: Action) => {
+      if (register[action.type]) {
         register[action.type].forEach(resolve => resolve(action));
         register[action.type] = [];
       }
       return next(action);
-    }
+    },
   };
-};
+}
 
 export default createAsyncFlowsMiddleware;
-
-
-
